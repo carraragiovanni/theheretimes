f4c230708ab413684e9041bc75253dfc4044cf00
carraragiovanni <carrara.g@gmail.com>
2019-02-26 21:08:12 -0500

    index

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
+app.use(express.static(__dirname + '/public'));
 
-var path = __dirname + '/public';
-var port = 1001;
+app.listen(process.env.PORT || 3006);
 
-app.use(express.static(path));
-app.get('*', function (req, res) {
-    res.sendFile(path + '/index.html');
-});
-app.listen(port);
+
+
+// var path = __dirname + '/public';
+// var port = 1001;
+
+// app.use(express.static(path));
+// app.get('*', function (req, res) {
+//     res.sendFile(path + '/index.html');
+// });
+// app.listen(port);
